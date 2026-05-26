c7a32de93059ede5841cf0ada0790466ba48be5e
kelvinSeamount <kelvinnwachukwu89@gmail.com>
2026-05-26 06:49:44 +0200

    Add initial Terraform configuration for dev environment setup with VPC,EKS, RDS, ECR, IAM, and Secrets Manager modules

---
+++ envs/dev/backend.tf
@@ -0,0 +1,11 @@
+# backend configuration
+
+terraform {
+  backend "s3" {
+    bucket       = "zen-pharma-tf-state-kelvinseamount"  # Replace with your S3 bucket name
+    key          = "envs/dev/terraform.tfstate"
+    region       = "eu-central-1"  # Replace with your AWS region
+    encrypt      = true
+    use_lockfile = true   # S3 native locking
+  }
+}
